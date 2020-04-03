a58526f63749a8b141b5ba50a6c0785a08813c0c
AkhterRasool <akhterrasool48@gmail.com>
2020-04-03 12:24:51 +0530

    Fix bug where nothing happens proper error message is not displayed when server is not up and running

--- app/src/main/java/com/akhterrasool/collegelibrary/clientrequest/ClientRequest.java
+++ app/src/main/java/com/akhterrasool/collegelibrary/clientrequest/ClientRequest.java
@@ -2,6 +2,7 @@ package com.akhterrasool.collegelibrary.clientrequest;
 
 import android.util.Log;
 
+import com.akhterrasool.collegelibrary.R;
 import com.akhterrasool.collegelibrary.activity.SearchActivity;
 import com.android.volley.Request;
 import com.android.volley.Response;
@@ -9,12 +10,16 @@ import com.android.volley.Response;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.util.Objects;
+
 import static com.akhterrasool.collegelibrary.app.Constants.JSON_MESSAGE_KEY;
+import static com.akhterrasool.collegelibrary.util.AppUtils.getResourceString;
 import static com.akhterrasool.collegelibrary.util.AppUtils.showLong;
 
 public abstract class ClientRequest<T> implements ResponseHandler<T> {
 
     private static final String TAG = "com.akhterrasool.collegelibrary.clientrequest.ClientRequest";
+    private static final String CANNOT_CONNECT_TO_SERVER = getResourceString(R.string.could_not_connect_to_server);
     protected String url;
     private boolean isProcessed = false;
     private boolean hasErrors = false;
@@ -53,10 +58,13 @@ public abstract class ClientRequest<T> implements ResponseHandler<T> {
         Response.ErrorListener errorListener = error -> {
             String message = null;
             try {
+                Objects.requireNonNull(error, CANNOT_CONNECT_TO_SERVER);
+                Objects.requireNonNull(error.networkResponse, CANNOT_CONNECT_TO_SERVER);
+                Objects.requireNonNull(error.networkResponse.data, CANNOT_CONNECT_TO_SERVER);
                 JSONObject errorData = new JSONObject(new String(error.networkResponse.data));
                 message = errorData.get(JSON_MESSAGE_KEY).toString();
-            } catch (JSONException e) {
-                e.printStackTrace();
+            } catch (JSONException | NullPointerException e) {
+                message = e.getMessage();
             }
             Log.e(SearchActivity.class.getName(), message);
             showLong(message);
